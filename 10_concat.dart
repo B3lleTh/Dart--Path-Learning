8feee7e7d15831da10d52c15afa290d7b22d00b4
Ghostar <gillomdev@gmail.com>
2026-01-27 13:08:07 -0700

    3rd Program - Concat Strings? lol

---
+++ 10_concat.dart
@@ -0,0 +1,14 @@
+import 'dart:io'; 
+main(){
+  print("--- 08 HI Program ---");
+  stdout.write("Type Your Name: ");
+  String? name = stdin.readLineSync();  
+
+  stdout.write("Type Your Name: ");
+  String? middleName = stdin.readLineSync();  
+
+  stdout.write("Type Your Name: ");
+  String? lastName = stdin.readLineSync();  
+  
+  print('Hi $name $middleName $lastName Nice to Know You!'); 
+}
