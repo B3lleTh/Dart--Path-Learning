84c86f9d2825acafeed9993a1862c648e0b84cd3
Ghostar <gillomdev@gmail.com>
2026-02-18 02:05:07 -0700

    Added New Practice Program

---
+++ mobileAppHws/potencias.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+int expo(int bases, exponent){
+
+  if(exponent <= 0){
+    return 1;
+  }
+
+  return bases * expo(bases,exponent - 1);
+
+}
+
+
+
+void main(){
+  print("=========== PROGRAMA DE PRACTICA POTENCIAS =========");
+
+  stdout.write("Ingresa tu Base: ");
+  String initialBase = stdin.readLineSync() ?? "";
+
+   stdout.write("Ingresa tu Exponente: ");
+  String initialExponent = stdin.readLineSync() ?? "";
+
+
+  int? parseInitialBase = int.tryParse(initialBase);
+
+  int? parseInitialExponent = int.tryParse(initialExponent);
+
+
+
+  int result = expo(parseInitialBase!, parseInitialExponent!);
+
+
+  print("El valor Final es: $result");
+}
